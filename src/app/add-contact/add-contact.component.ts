72384c43a71ad7612a316d9667663e62c37a4ee3
mordechaikatz <mordyk123@gmail.com>
2023-08-09 08:45:55 -0400

    first commit

---
+++ src/app/add-contact/add-contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-contact',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-contact.component.html',
+})
+export class AddContactComponent {
+
+
+}
